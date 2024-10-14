71574f5161355b85908e5dce89003ae30710773a
Sharmaketan <ketan.sharma@iensure.in>
2024-10-14 17:17:31 +0530

    add: Controllers, Middleware and basic CRUD operations added

---
+++ middleware/logger.js
@@ -0,0 +1,8 @@
+// Logger middleware
+
+const logger = (req, res, next) => {
+  console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`)
+  next()
+}
+
+module.exports = logger
--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const colors = require("colors")
+const connectDB = require("./config/db")
+const logger = require("./middleware/logger")
+const morgan = require("morgan")
+
+//Route files
+
+const bootcamps = require("./routes/bootcamps")
 
 dotenv.config({ path: "./config/config.env" })
 
+//Connect to database
+connectDB()
+
 const app = express()
+
+//Body parser
+app.use(express.json())
+
+// DEV logging middleware
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"))
+}
+
+//Mount routers
+
+app.use("/api/v1/bootcamps", bootcamps)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`.yellow.bold)
+})
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err}`.red)
+  server.close(() => process.exit(1))
 })
