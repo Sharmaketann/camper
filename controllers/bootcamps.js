24e6c79cb818bd729cb7e3d6b3d5c6a4e4112f3a
Sharmaketan <ketan.sharma@iensure.in>
2024-10-22 14:16:22 +0530

    add: Pagination added with next and previous page within the response

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -42,18 +42,39 @@ exports.getBootcamps = asyncHand1er(async (req, res, next) => {
     query = query.sort("-createdAt")
   }
 
-  // Pagination
+  // Pagination  page=2&limit=10  1-10 11-20
   const page = parseInt(req.query.page, 10) || 1
   const limit = parseInt(req.query.limit, 10) || 25
   const startIndex = (page - 1) * limit
   const endIndex = page * limit
+
   const total = await Bootcamp.countDocuments()
+
+  query = query.skip(startIndex).limit(limit)
+
+  // Pagination result
+  const pagination = {}
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    }
+  }
 
   // Executing query
   const bootcamps = await query
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps,
   })
 })
