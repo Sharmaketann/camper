72699ba9e648c493ed0876cc82537ff55a0fe290
Sharmaketan <ketan.sharma@iensure.in>
2024-10-22 14:00:16 +0530

    add: Sorting and Selection functionality added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,9 +9,47 @@ const geocoder = require("../utils/geocoder")
 
 exports.getBootcamps = asyncHand1er(async (req, res, next) => {
   let query
-  let queryStr = JSON.stringify(req.query)
+
+  // Copy req.query
+  let reqQuery = { ...req.query }
+
+  // Fields to exclude
+  const removeFields = ["select", "sort", "page", "limit"]
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param])
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery)
+
+  // Create operator ($gt, $gte, etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
+
+  // Finding resources
   query = Bootcamp.find(JSON.parse(queryStr))
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ")
+    query = query.select(fields)
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ")
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort("-createdAt")
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 25
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+  const total = await Bootcamp.countDocuments()
+
+  // Executing query
   const bootcamps = await query
   res.status(200).json({
     success: true,
